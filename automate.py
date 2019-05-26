0114d33ba4a4e6e49f20cbcd6f6d71066ce29391
pearsonkyle <pearsonk@email.arizona.edu>
2019-05-26 14:59:24 -0700

    automation

--- automate.py
+++ automate.py
@@ -1,3 +1,13 @@
 from subprocess import call
-call("git pull origin master",shell=True)
-call("python twitter_post.py",shell=True)
+import argparse
+import time
+
+parser = argparse.ArgumentParser()
+help_ = "Sleep time"
+parser.add_argument("-s", "--sleep", help=help_, default=24*60*60, type=int)
+args = parser.parse_args()
+
+while(True):
+    call("git pull origin master",shell=True)
+    call("python twitter_post.py",shell=True)
+    time.sleep(args.sleep)
