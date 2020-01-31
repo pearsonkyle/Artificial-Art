7038592744422d22f86f66d174aabb71e8cc4452
Kyle A. Pearson <pearsonk@email.arizona.edu>
2020-01-31 09:31:37 -0700

    Add files via upload

--- space_train.py
+++ space_train.py
@@ -21,15 +21,15 @@ if __name__ == '__main__':
     if args is None:
       exit()
 
-    x_train, y_train = create_dataset(128, 128, nSlices=1000, resize=0.10, directory='Space/') # 3 channels = RGB
+    x_train, y_train = create_dataset(64, 64, nSlices=1000, resize=0.5, directory='Space/Galaxy/') # 3 channels = RGB
     assert(x_train.shape[0]>0)
 
     x_train /= 255 
 
     # plot results to make sure data looks good!
-    fig, axs = plt.subplots(4, 4)
-    for i in range(4):
-        for j in range(4):
+    fig, axs = plt.subplots(10, 10)
+    for i in range(10):
+        for j in range(10):
             axs[i,j].imshow( x_train[ np.random.randint(x_train.shape[0]) ] )
             axs[i,j].axis('off')
     plt.show()
@@ -37,10 +37,14 @@ if __name__ == '__main__':
     dcgan = DCGAN(img_rows = x_train[0].shape[0],
                     img_cols = x_train[0].shape[1],
                     channels = x_train[0].shape[2], 
-                    latent_dim=256,
-                    name='space2_256_128')
+                    latent_dim=128,
+                    name='space_128_64')
     try:
-      dcgan.load_weights(generator_file="generator ({}).h5".format(dcgan.name), discriminator_file="discriminator ({}).h5".format(dcgan.name) )
-    except:
-      pass                  
-    dcgan.train(x_train, epochs=args.epochs, batch_size=32, save_interval=100)
+        dcgan.load_weights(
+            generator_file="generator ({}).h5".format(dcgan.name), 
+            discriminator_file="discriminator ({}).h5".format(dcgan.name) 
+        )
+    except Exception as e:
+        print("failed to load weights:",e)
+
+    dcgan.train(x_train, epochs=args.epochs, batch_size=32, save_interval=1000)
